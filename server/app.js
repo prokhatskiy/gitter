4389a480a0ecaa759dee5489025be2a878c991e3
Artem Prokhatskyi <artem.p@siteplus.com>
2018-06-11 08:03:58 +0300

    added port

--- server/app.js
+++ server/app.js
@@ -121,4 +121,4 @@ app.get('/*', function(req, res) {
 });
 
 // Start server
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
